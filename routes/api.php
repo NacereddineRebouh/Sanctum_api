8022697f419626f55222c5cce44e0090d139ba8b
Nacereddine_rbh <llbl2153@gmail.com>
2022-12-18 10:52:29 +0100

    Main api.php

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API Routes SanctumOnly
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
