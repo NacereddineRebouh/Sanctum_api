2dc6aaaa7bcc096729542cdce225227a2819af70
Nacereddine_rbh <llbl2153@gmail.com>
2022-12-18 10:50:49 +0100

    SancSocialite

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API Routes//SancSoc
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
